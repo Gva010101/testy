d8ed80271c7c8b4464f7ae9a11bd7b997b114782
Vladimir <gvadel0106@gamil.com>
2024-11-04 14:09:27 +0300

    8

--- testy/testy/ContentView.swift
+++ testy/testy/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("7")
+            Text("8")
 
 
 

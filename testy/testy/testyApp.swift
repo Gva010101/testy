3d8eecc5b593d5a141924c17a038daece6a2ff76
Vladimir <gvadel0106@gamil.com>
2024-11-04 13:38:04 +0300

    1

---
+++ testy/testy/testyApp.swift
@@ -0,0 +1,17 @@
+//
+//  testyApp.swift
+//  testy
+//
+//  Created by Vladimir Grishchenkov on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct testyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
